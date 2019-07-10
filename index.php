5466ee4133d4a68f05b7e4ab9840065151133cc1
fjimenezJOB <fjimenezjob@gmail.com>
2019-07-10 11:46:08 +0200

    clase de Randomize creada, funcion de nombres random creada

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+echo'';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Document</title>
+</head>
+<body>
+
+<!-- HEADER -->
+
+<header>
+
+    <nav>
+        <ul>
+
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+
+        </ul>
+
+    </nav>
+
+</header>
+
+<!-- SECTION -->
+
+<section>
+
+    <article>
+
+    </article>
+
+
+</secrtion>
+
+<!-- FOTER -->
+
+<footer>
+
+
+</footer>
+
+    
+</body>
+</html>
---
+++ models/Randomize.php
@@ -0,0 +1,54 @@
+<?php
+
+class Randomize{
+
+
+    // Constructor
+
+    public function __construct(){
+
+
+
+
+    }
+
+    //Metodos
+
+
+    public function nombresRandom(){
+
+        $nombres=[
+
+            'Fran',
+            'Petro',
+            'Alejandro',
+            'Lolo',
+            'Cristian',
+            'Alexis',
+            'Luis',
+            'David',
+            'Jordi',
+            'Jorge',
+            'Julia',
+            'Jose',
+            'Mateo',
+            'Jaume'
+        ];
+
+
+        $nombre = rand(0,14);
+
+
+        print_r
+
+
+
+
+
+    }
+
+
+
+}
+
+?>
