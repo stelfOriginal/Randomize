e64de97375146b13622dc3ac207b323ae7cbb420
alejandro <lstelf32@gmail.com>
2019-07-10 13:31:24 +0200

    pues he creado todo desde el principio ya que no me llego el archivo index totalmente en blanco

--- index.php
+++ index.php
@@ -1,57 +1,55 @@
+<!-- PHP 7 -->
+
 <?php
-echo'';
+
+function generar_nombre_aleatorio(&$nombres, &$apellidos){
+	$nombreAleatorio = $nombres[ mt_rand(0, count($nombres) -1) ];
+	$apellidoAleatorio = $apellidos[ mt_rand(0, count($apellidos) -1) ];
+
+	return "$nombreAleatorio - $apellidoAleatorio ";
+}
 ?>
+
+<!-- HTML 5 -->
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
-
-    <title>Document</title>
 </head>
 <body>
+<!-- header -->
+    <header>
 
-<!-- HEADER -->
+    </header>
+    <!-- section -->
 
-<header>
+    <section>
+        <article></article>
+    </section>
+    
 
-    <nav>
-        <ul>
-
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-
-        </ul>
-
-    </nav>
-
-</header>
-
-<!-- SECTION -->
-
-<section>
-
-    <article>
-
-    </article>
+<?php
 
 
-</secrtion>
+$nombres = ["jorge", "Julia", "Jose", "Mateo", "Jaume", "Petro", "Alejandro", ];
+$apellidos = ["Fran","Adri","Lolo","Cristian","Jordi","Luis","David","Tony"];
+ 
+$nombre = generar_nombre_aleatorio($nombres, $apellidos);
+echo $nombre; // Salida de ejemplo -> pedro smith pérez
+?>
 
-<!-- FOTER -->
-
-<footer>
-
-
-</footer>
 
     
+    <!-- footer -->
+
+    <footer>
+    
+    </footer>
 </body>
 </html>
